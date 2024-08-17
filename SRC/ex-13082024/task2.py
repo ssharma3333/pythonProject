49a8b715db0fce4abdc5349b0181fa00771a5a2f
Swati Sharma <ssharma3333@gmail.com>
2024-08-17 13:31:36 +0530

    Task and task 2 push in git

---
+++ SRC/ex-13082024/task2.py
@@ -0,0 +1,28 @@
+# create 2 input
+num1= int(input("enter the first number-"))
+num2= int(input("enter the secound number-"))
+
+Maximum=max(num1,num2)
+print(Maximum)
+Power=pow(num1,num2)
+print(Power)
+sum=(num1+num2)
+print(sum)
+sub=(num1-num2)
+print(sub)
+mul=(num1*num2)
+print(mul)
+div=(num1/num2)
+print(div)
+
+print(f"max no of 2 is- {Maximum}")
+print(f'power of 2 is -{Power}')
+print(f"sum of 2 no is-{sum}")
+print(f"sub of 2 no is-{sub}")
+print(f"mul of 2 no is-{mul}")
+print(f"div of 2 no is-{div}")
+
+print("sum of two no is", (sum))
+print("sum of two no is", (num1*num2))
+
+print(f"sum (num1 + num2): {sum}")
